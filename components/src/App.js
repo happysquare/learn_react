a72ea45f2f590c4bf30741acc1abbe5446e63248
James Johnston <james@happysquare.com.au>
2022-09-29 21:05:04 +0200

    State in app, components update via properties

--- components/src/App.js
+++ components/src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
 import {useState} from 'react'
 
-function MyButton(){
-  const [count, setCount] = useState(0);
-
-  function handleClick(){
-    setCount(count +1);
-  }
+function MyButton({count, onClick}){
 
   return (
-    <button onClick={handleClick} >
+    <button onClick={onClick}>
         Clicked {count} times
     </button>
   );
 }
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count +1);
+  }
+
   return(
     <div>
-      <h3>These buttons count independantly</h3>
-    <MyButton />
-    <MyButton />
+      <h3>These buttons count together</h3>
+    <MyButton onClick={handleClick} count={count}/>
+    <MyButton onClick={handleClick} count={count}/>
     </div>
   );
 }
