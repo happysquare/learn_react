49c1e45a7613babc2a222f92cbf14ba1ae97e41b
James Johnston <james@happysquare.com.au>
2022-09-29 20:59:19 +0200

    buttons maintaining state

--- components/src/App.js
+++ components/src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import {useState} from 'react'
+
+function MyButton(){
+  const [count, setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count +1);
+  }
+
+  return (
+    <button onClick={handleClick} >
+        Clicked {count} times
+    </button>
+  );
+}
 
 function App() {
   return(
-    <h1>hi</h1>
+    <div>
+      <h3>These buttons count independantly</h3>
+    <MyButton />
+    <MyButton />
+    </div>
   );
 }
 
